58519451b6a3549c7e5f0659235c5c17d05249f6
HarshavardhanReddy00 <harshavardhanreddy19027c001@gmail.com>
2026-04-14 22:25:18 +0530

    added fetch for particular product ID

---
+++ src/pages/ProductDetail.jsx
@@ -0,0 +1,19 @@
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+
+export default function ProductDetail() {
+  const { id } = useParams();
+  const [product, setProduct] = useState(null);
+
+  useEffect(() => {
+    fetch(`https://dummyjson.com/products/${id}`)
+      .then(res => res.json())
+      .then(setProduct)
+      .catch(() => console.log("Error"));
+  }, [id]);
+
+ return (
+    <>
+    </>
+ );
+}
