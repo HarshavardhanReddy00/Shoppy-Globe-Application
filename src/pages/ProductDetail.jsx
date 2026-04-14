9af75335d1f4d4f979c112d7ed24f88d5be131fe
HarshavardhanReddy00 <harshavardhanreddy19027c001@gmail.com>
2026-04-14 22:30:11 +0530

    added details of that product to display

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -12,8 +12,16 @@ export default function ProductDetail() {
       .catch(() => console.log("Error"));
   }, [id]);
 
- return (
-    <>
-    </>
- );
+  if (!product) return <h1>Loading...</h1>;
+
+  return (
+    <div className="p-4 flex gap-4 bg-purple-500">
+      <img src={product.thumbnail} className="w-60" />
+      <div>
+        <h1 className="text-xl">{product.title}</h1>
+        <p>{product.description}</p>
+        <p>₹{product.price}</p>
+      </div>
+    </div>
+  );
 }
