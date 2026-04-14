f24486818b021f7d692865835965eafb44f6f9dd
HarshavardhanReddy00 <harshavardhanreddy19027c001@gmail.com>
2026-04-14 22:14:18 +0530

    added remaining reducers for cart

--- src/redux/cartSlice.js
+++ src/redux/cartSlice.js
@@ -16,6 +16,27 @@ const cartSlice = createSlice({
         state.items.push({ ...action.payload, quantity: 1 });
       }
     },
+    
+    removeFromCart: (state, action) => {
+      state.items = state.items.filter(i => i.id !== action.payload);
+    },
+
+    increaseQty: (state, action) => {
+      const item = state.items.find(i => i.id === action.payload);
+      if (item) item.quantity++;
+    },
+
+    decreaseQty: (state, action) => {
+      const item = state.items.find(i => i.id === action.payload);
+      if (item && item.quantity > 1) item.quantity--;
+    },
+
+    clearCart: (state) => {
+      state.items = [];
+    }
 },
 });
+
+export const { addToCart, removeFromCart, increaseQty, decreaseQty, clearCart } = cartSlice.actions;
+export default cartSlice.reducer;
 
