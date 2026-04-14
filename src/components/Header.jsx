4e21f98591e0306ede42a9de21b1906db35f42dd
HarshavardhanReddy00 <harshavardhanreddy19027c001@gmail.com>
2026-04-14 23:01:32 +0530

    added Home,Cart and Checkout link to navigate

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,6 +6,8 @@ export default function Header() {
   const dispatch = useDispatch();
 
   const items = useSelector(state => state.cart.items);
+
+  const totalCount = items.reduce((total, item) => total + item.quantity, 0);
   
 
   return (
@@ -16,6 +18,17 @@ export default function Header() {
         className="border px-2 py-1"
         placeholder="Search products..."
         onChange={(e) => dispatch(setSearch(e.target.value))}/>
+
+        <div className="space-x-4">
+        <Link to="/" className="hover:bg-yellow-400">Home</Link>
+        <Link to="/cart" className="hover:bg-yellow-400">🛒 Cart
+          {totalCount > 0 && (
+            <span className="ml-1 bg-red-500 text-white text-xs px-2 py-0.5 rounded-full">
+              {totalCount}
+            </span>
+          )}</Link>
+        <Link to="/checkout" className="hover:bg-yellow-400">Checkout</Link>
+      </div>
       
     </div>
   );
